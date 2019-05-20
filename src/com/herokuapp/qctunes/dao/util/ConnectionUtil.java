8ddad5ba9e048ed02416d7d4c033fee76ecfd5d1
Kim Nguyen <kimnnguyen225@gmail.com>
2019-05-19 23:23:04 -0400

    made changes in database utility

--- src/com/herokuapp/qctunes/dao/util/ConnectionUtil.java
+++ src/com/herokuapp/qctunes/dao/util/ConnectionUtil.java
@@ -20,9 +20,9 @@ public class ConnectionUtil {
 //        String dbUrl = "jdbc:mysql://localhost:3306/qctunes";
 //        String user = "root";
 //        String pw = "root";
-        String dbUrl = "jdbc:postgresql://" + "ec2-54-225-106-93.compute-1.amazonaws.com:5432" +
-                "/d2ihg7f4ifpm3v?sslmode=require"; //?sslmode=require, it's to connect to
-        // database from your machine
+        String dbUrl = "jdbc:postgresql://ec2-54-225-106-93.compute-1.amazonaws.com:5432/d2ihg7f4ifpm3v?ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory";
+        //?sslmode=require, it's to connect to database from your machine
+        // ?ssl=true&sslfactory=org.postgresql.ssl.NonValidatingFactory
         String user = "zpfymqztsbzhvc";
         String pw = "a7ea039d9bfc9244aeb51aa0a4f83b2de19a29b85deec17e2b5f792a100dd9e8";
         return DriverManager.getConnection(dbUrl, user, pw);
